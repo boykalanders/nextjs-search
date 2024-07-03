a89a1e0983d2d8dbf8a21605b7084fc32b55ce0a
Boykalanders <boykalanders@gmail.com>
2024-07-03 02:30:55 -0700

    refactor: downgrade @heroicons & fix class to className

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GlobeIcon } from '@heroicons/react/solid'
+// import { GlobeIcon } from '@heroicons/react/solid'
 
 function Footer() {
     return (
@@ -9,7 +9,7 @@ function Footer() {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
                 <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
-                    <GlobeIcon className="h-5 mr-1 text-green-700" /> <p className='link'>Carbon neutral since 2007</p> 
+                    {/* <GlobeIcon className="h-5 mr-1 text-green-700" /> <p className='link'>Carbon neutral since 2007</p>  */}
                 </div>
                 <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
                     <p className='link'>Advertising</p>
